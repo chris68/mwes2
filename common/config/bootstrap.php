12895ae8a2875dacbdfc7926e6e57529c63fcf73
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-12-21 12:06:58 +1100

    Fix encoding

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,6 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+
+// Our website is all utf-8; so we better make sure the default mb_internal_encoding is utf-8 as well; otherwise routines like mb_strtolower won't work correctly
+mb_internal_encoding('UTF-8');
