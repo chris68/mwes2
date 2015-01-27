2cedab5dee2fb982bd695e6b20ec3b275656125f
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2015-01-27 15:43:54 +1100

    Improved deploy for /var/opt/....

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 env=""
-vendor=""
 stage=""
 branch="master"
-while getopts "b:sve:" optname
+while getopts "b:se:" optname
   do
     case "$optname" in
       "e")
@@ -24,9 +23,6 @@ while getopts "b:sve:" optname
       "b")
         branch=$OPTARG
         ;;
-      "v")
-        vendor="yes"
-        ;;
       "s")
         stage="yes"
         ;;
@@ -42,7 +38,7 @@ while getopts "b:sve:" optname
     esac
   done
 if [ "$env" == "" ]; then
-	echo "Parameter --env must be given"
+	echo "Parameter --e must be given"
 	exit 1
 fi
 echo Deploying to $env
@@ -51,17 +47,16 @@ git clone https://github.com/chris68/mwes2 /home/mailwitch/mwes2$suffix
 { cd /home/mailwitch/mwes2$suffix; git checkout $branch; }
 # psql postgres #create the database (see migration)
 # psql postgres #CREATE DATABASE mwes2_dev WITH TEMPLATE mwes2; (for Development test)
-if [ "$vendor" == "yes" ]; then
-	sudo composer.phar self-update
-	composer.phar global require "fxp/composer-asset-plugin:1.0.0-beta4"
-	composer.phar create-project -d /home/mailwitch/mwes2$suffix 
+sudo composer self-update
+composer global require "fxp/composer-asset-plugin:1.0.0-beta4"
+composer create-project -d /home/mailwitch/mwes2$suffix 
 fi
 /home/mailwitch/mwes2$suffix/init --env=$env
 /home/mailwitch/mwes2$suffix/yii migrate
 if [ "$stage" == "yes" ]; then
-	sudo rm -R /opt/mailwitch/www/mwes2$suffix
-	sudo cp -R /home/mailwitch/mwes2$suffix /opt/mailwitch/www/.
-	sudo rm -R /opt/mailwitch/www/mwes2$suffix/.git
-	sudo chown -R www-data:mailwitch /opt/mailwitch/www/mwes2$suffix
+	sudo rm -R /var/opt/mailwitch/www/mwes2$suffix
+	sudo cp -R /home/mailwitch/mwes2$suffix /var/opt/mailwitch/www/.
+	sudo rm -R /var/opt/mailwitch/www/mwes2$suffix/.git
+	sudo chown -R www-data:mailwitch /var/opt/mailwitch/www/mwes2$suffix
 fi
 # rm -R -f /home/mailwitch/mwes2$suffix #remove again.
