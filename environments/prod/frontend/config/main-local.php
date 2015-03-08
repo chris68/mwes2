efc07f439f37eca133f9400405769d8069379bbf
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2015-03-08 19:04:17 +0100

    Duplicate mailer config (already in common)

--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -5,9 +5,5 @@ return [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-        ],
     ],
 ];
