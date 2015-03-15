7e25a9710611545627c235812950f1691cbef359
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2015-03-15 23:49:12 +0100

    Postfix configuration

---
+++ hosting/postfix/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ "$UID" -ne "0" ]; then
+  echo You must be root to run this script.
+  exit 1
+fi
+
+function check_error()
+{
+  local exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo "$PWD [$0]: Last command failed (exit code $exit_code) - now aborting"
+    exit 1
+  fi
+}
+
+if test -e /etc/postfix/mailwitch; then
+  rm -R /etc/postfix/mailwitch || check_error
+fi
+mkdir /etc/postfix/mailwitch || check_error
+chown postfix:postfix /etc/postfix/mailwitch || check_error
+chmod 755 /etc/postfix/mailwitch  || check_error
+
+cp mailwitch/*.{hash,regexp,pgsql} /etc/postfix/mailwitch/. || check_error
+# Append the config file in order not to overwrite the existing one
+cat main.cf >> /etc/postfix/main.cf || check_error
+
+# postfix must be able to read the files: but only postfix should be allowed
+chown postfix:postfix /etc/postfix/mailwitch/* || check_error
+chmod 664 /etc/postfix/mailwitch/*  || check_error
+
+# Build up the mappings db (currently not needed)
+#postmap /etc/postfix/mailwitch/*.hash || check_error
---
+++ hosting/postfix/mailwitch/virtual_alias_maps.pgsql
@@ -0,0 +1,21 @@
+#
+# pgsql config file for local(8) aliases(5) lookups
+#
+
+# The user name and password to log into the psql server.
+user = mailwitch
+password = mailwitch
+
+# The database name on the servers.
+dbname = mwes2
+
+# The table name.
+table = PostfixRecipientAliases
+
+# Query components, see below.
+select_field = Target
+where_field = Source
+
+# You may specify additional_conditions or leave this empty.
+additional_conditions = 
+
