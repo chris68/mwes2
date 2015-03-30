5217965b79a85e86c745379f1d8b4cd447834366
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2015-03-30 18:09:42 +0200

    Prepare for bug fix release 2.0.1

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name' => 'Mailwitch Email Services',
-    'version' => '2.0',
+    'version' => '2.0.1',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
