2c7fe34f260454632fd05e32cc96887500a5d26c
Alexander Makarov <sam@rmcreative.ru>
2015-06-17 23:49:18 +0200

    Fixes #8762: Moved Alert widget to common, used it in both frontend and backend layouts
    (cherry picked from commit a38f71c4a2381b49c9338b028e96fe1a76d37a00)
    
    Conflicts:
    	frontend/views/layouts/main.php

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -5,7 +5,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use frontend\widgets\Alert;
+use common\widgets\Alert;
 use frontend\models\Emaildomain;
 
 /* @var $this \yii\web\View */
