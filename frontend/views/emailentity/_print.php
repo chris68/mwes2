82a63a0f45dbf05710c81ff7fc3081bb9f71ef56
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2016-12-31 01:23:36 +0100

    Use tel tag also for print

--- frontend/views/emailentity/_print.php
+++ frontend/views/emailentity/_print.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use frontend\helpers\ModelFormatter;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Emailentity */
@@ -8,7 +9,7 @@ use yii\helpers\Html;
 
 <span class="lead"><?= Html::encode($model->sortname)?></span>
 <span style="font-weight: bold">&lt;<?= Html::encode($model->getCompleteEmailname())?>&gt;</span>
-<p><?= nl2br(Html::encode($model->comment))?></p>
+<p><?=ModelFormatter::formatwithHtmlTelTags($model->emaildomain,$model->comment)?></p>
 <p>
     <?php foreach ($model->emailmappings as $mapping) : ?>
     <b><?=$mapping->emailarea->name?><?=$mapping->locked?' (gesperrt): ':': '?></b>
