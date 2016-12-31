ed4097a9977e29c14a4327e0d1f4a862771efe95
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2016-12-31 01:24:06 +0100

    Prepare for release 2.1.2

--- common/config/main.php
+++ common/config/main.php
@@ -2,7 +2,7 @@
 return [
 // @chris68
     'name' => 'Mailwitch Email Services',
-    'version' => '2.1.1',
+    'version' => '2.1.2',
 
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
