c93573ab037ae6ebc7228b317c8f3f909daf92b1
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2017-01-01 15:11:16 +0100

    Updated release mail

--- common/mail/notification-release.php
+++ common/mail/notification-release.php
@@ -9,12 +9,10 @@ Hallo <?=$user->username?>!
 
 Bei der Webplattform mailwitch.com ist soeben die neue Release <?=Yii::$app->version?> veröffentlicht worden. Die Neuerungen finden Sie in der Releasehistorie (<?=Yii::$app->urlManager->createAbsoluteUrl(['site/releasehistory'])?>).
 
-Wegen einer notwendigen technischen Umstellung müssen Sie ihr Passwort zurücksetzen. Dies können Sie über den Link <?=Yii::$app->urlManager->createAbsoluteUrl(['site/request-password-reset'])?> einleiten.
-
-Auch sollten Sie sich danach in der Nutzerverwaltung <?=Yii::$app->urlManager->createAbsoluteUrl(['site/userdata'])?> einen einfacheren Nutzernamen holen, da Sie diesen nun dauerhaft für die Anmeldung brauchen.
-
-Bitte beachten Sie auch die aktualisierten AGB (<?=Yii::$app->urlManager->createAbsoluteUrl(['site/terms'])?>) und Datenschutzbedingungen (<?=Yii::$app->urlManager->createAbsoluteUrl(['site/privacy'])?>). Die wichtigste Neuerung ist hier, dass Nutzer nach einem Jahr Untätigkeit gelöscht werden.
+Es würde uns freuen, wenn wir Sie bald mal wieder auf unserer Plattform begrüßen dürften.
 
 ===================================================================================================
 
 Sie bekommen diese Email, weil Sie bei mailwitch.com unter dem Nutzer <?=$user->username?> mit der Emailadresse <?=$user->email?> registriert sind. Wenn Sie mailwitch.com nicht mehr nutzen wollen, dann löschen Ihren Nutzer bitte in der Nutzerverwaltung.
+
+Bitte antworten Sie nicht auf diese Mail, sondern nutzen Sie stattdessen bitte die Kontaktseite (<?=Yii::$app->urlManager->createAbsoluteUrl(['site/contact'])?>)
