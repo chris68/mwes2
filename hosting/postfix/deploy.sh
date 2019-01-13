3f1f2c7fdca957651f915e419ba93b10460245ca
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2019-01-13 13:31:17 +0100

    Make postfix ready for IPV6 and the relevant postfix version in ubuntu 18.04

--- hosting/postfix/deploy.sh
+++ hosting/postfix/deploy.sh
@@ -17,19 +17,24 @@ if test -e /etc/postfix/mailwitch; then
   rm -R /etc/postfix/mailwitch || check_error
 fi
 mkdir /etc/postfix/mailwitch || check_error
-chown postfix:postfix /etc/postfix/mailwitch || check_error
+chown root:root /etc/postfix/mailwitch || check_error
 chmod 755 /etc/postfix/mailwitch  || check_error
 
 cp mailwitch/*.{hash,regexp,pgsql} /etc/postfix/mailwitch/. || check_error
 # Append the config file in order not to overwrite the existing one
 cat main.cf >> /etc/postfix/main.cf || check_error
 
-# postfix must be able to read the files: but only postfix should be allowed
-chown postfix:postfix /etc/postfix/mailwitch/* || check_error
-chmod 664 /etc/postfix/mailwitch/*  || check_error
+# postfix must be able to read the files; no one should be allowed to write the files
+chown root:root /etc/postfix/mailwitch/* || check_error
+chmod 444 /etc/postfix/mailwitch/*  || check_error
 
 # Build up the mappings db (currently not needed)
 #postmap /etc/postfix/mailwitch/*.hash || check_error
 
-echo "Postfix configuration deployed. Do not forget to edit/reconcile the configuration file via"
+echo "Postfix configuration deployed. "
+echo "The configuration section for mailwitch has been appended (!) to the end of /etc/postfix/main.cf"
+echo "Do not forget to edit/reconcile the configuration file via"
 echo "sudo nano /etc/postfix/main.cf"
+echo "You must ensure that no config parameter is set twice" via
+echo "sudo service postfix reload; sudo tail -f /var/log/mail.log"
+echo "The log should not show any warnings"
