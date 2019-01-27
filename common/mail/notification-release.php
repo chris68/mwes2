876440a185419b9534e3483b9706f0d9d4e08ca0
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2019-01-27 15:28:29 +0100

    Release letter with terms and privacy changes

--- common/mail/notification-release.php
+++ common/mail/notification-release.php
@@ -9,6 +9,8 @@ Hallo <?=$user->username?>!
 
 Bei der Webplattform mailwitch.com ist soeben die neue Release <?=Yii::$app->version?> veröffentlicht worden. Die Neuerungen finden Sie in der Releasehistorie (https://mailwitch.com/site/releasehistory).
 
+Eventuell wurden auch die Nutzungsbedingungen (https://mailwitch.com/site/terms) oder Datenschutzbedingungen (https://mailwitch.com/site/privacy) geändert. Am besten lesen Sie sich diese daher noch einmal schnell durch, ob Sie immer noch mit allem einverstanden sind.
+
 Es würde uns freuen, wenn wir Sie bald mal wieder auf unserer Plattform begrüßen dürften.
 
 ===================================================================================================
